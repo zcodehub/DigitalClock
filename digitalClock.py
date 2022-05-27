bb5f1afa9a06ea9597363775e86e3251ecfa8c36
zcode <sqlpractice19@gmail.com>
2022-05-26 23:16:28 -0400

    changes made

--- digitalClock.py
+++ digitalClock.py
@@ -3,5 +3,7 @@ from tkinter import Tk, Label
 window = Tk()
 window.title("Digital Clock")
 window.geometry("600x300")
+window.configure(bg="steelblue")
+Label = Label(window, text="Welcome", font=("arial black",78,"bold"), bg="steelblue", fg="white")
 
 window.mainloop() 
