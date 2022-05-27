bc9f7e2967499094d77ceeade296707d7eab9299
zcode <sqlpractice19@gmail.com>
2022-05-27 00:01:44 -0400

    welcome added

--- digitalClock.py
+++ digitalClock.py
@@ -5,5 +5,5 @@ window.title("Digital Clock")
 window.geometry("600x300")
 window.configure(bg="steelblue")
 Label = Label(window, text="Welcome", font=("arial black",78,"bold"), bg="steelblue", fg="white")
-
+Label.pack(pady=100)
 window.mainloop() 
